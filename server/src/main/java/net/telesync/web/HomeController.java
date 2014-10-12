6085d0ffbaa327f9964c09c6940462ab5cbe931e
Aleksandr Oksenenko <crew4ok@gmail.com>
2014-10-12 17:06:40 +0300

    Add test controller

---
+++ server/src/main/java/net/telesync/web/HomeController.java
@@ -0,0 +1,17 @@
+package net.telesync.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ResponseEntity<String> returnHomePage() {
+        return new ResponseEntity<>("hello, world", HttpStatus.OK);
+    }
+
+}
