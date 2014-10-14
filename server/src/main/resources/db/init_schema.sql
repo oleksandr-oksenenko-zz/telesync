e1eac4f68551c23a6b8fd9f97286fec159ccf9f2
Aleksandr Oksenenko <crew4ok@gmail.com>
2014-10-14 12:48:01 +0300

    Fix line endings

--- server/src/main/resources/db/init_schema.sql
+++ server/src/main/resources/db/init_schema.sql
@@ -1,7 +1,7 @@
-create table if not exists devices (
-  id integer primary key,
-  url text,
-  device_name text,
-  tv_name text,
+CREATE TABLE IF NOT EXISTS devices (
+  id INTEGER PRIMARY KEY,
+  url TEXT,
+  device_name TEXT,
+  tv_name TEXT,
   last_heartbeat timestamp
 );
